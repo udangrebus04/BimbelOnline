f94ef8ec4b49afd8a60fe57553b4134664dba0ea
udangrebus04 <cellomitaputri@gmail.com>
2024-07-11 00:04:48 +0700

    adjust pricing section (sementara)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -91,18 +91,15 @@
                 <div class="bg-white dark:bg-gray-800">
                     <div class="container px-6 py-8 mx-auto">
                         <div class="flex flex-col items-center justify-center space-y-8 lg:-mx-4 lg:flex-row lg:items-stretch lg:space-y-0">
-                            <div class="flex flex-col w-full max-w-sm p-8 space-y-8 text-center bg-white border-2 border-gray-200 rounded-lg lg:mx-4 dark:bg-gray-800 dark:border-gray-700">
+                            <div class="flex flex-col w-full max-w-sm p-10 space-y-10 text-center bg-white border-2 border-gray-200 rounded-lg lg:mx-4 dark:bg-gray-800 dark:border-gray-700">
                                 <div class="flex-shrink-0">
                                     <h2 class="inline-flex items-center justify-center px-2 font-semibold tracking-tight text-blue-400 uppercase rounded-lg bg-gray-50 dark:bg-gray-700">
-                                        Casual
+                                        Paket 1 Bulan
                                     </h2>
                                 </div>
                                 <div class="flex-shrink-0">
                                     <span
-                                        class="pt-2 text-4xl font-bold text-gray-800 uppercase dark:text-gray-100"
-                                    >
-                                        Free
-                                    </span>
+                                        class="pt-2 text-4xl font-bold text-gray-800 uppercase dark:text-gray-100">Kasual</span>
                                 </div>
                                 <ul class="flex-1 space-y-4">
                                     <li class="text-gray-500 dark:text-gray-400">
@@ -119,7 +116,7 @@
                                 <button
                                     class="inline-flex items-center justify-center px-4 py-2 font-semibold text-white uppercase transition-colors bg-blue-500 rounded-lg hover:bg-blue-700 focus:outline-none"
                                 >
-                                    Start free
+                                    Rp.185.000/bulan
                                 </button>
                             </div>
             
