05cf66f8944775accbed919f09299877347e3a7d
andienip <andieniputri4@gmail.com>
2024-07-18 15:21:15 +0700

    tambah data

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,10 +1,12 @@
-@extends('layouts.app')
+@extends('layouts.header')
 
-@section('content')
+@section('header')
+@vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card mt-5">
                 <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
