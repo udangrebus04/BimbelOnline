1d1576375f818b0d2a5833ca4a775e95c143cb3d
udangrebus04 <cellomitaputri@gmail.com>
2024-07-19 13:07:31 +0700

    siswa index

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -25,9 +25,73 @@
             <div class="flex ">
                 <a href="#"
                    class=" block text-md px-4  ml-2 py-2 rounded text-indigo-700 font-bold hover:text-white mt-4 hover:bg-indigo-700 lg:mt-0">Akun</a>
-                <a href="#"
-                   class=" block text-md px-4  ml-2 py-2 rounded text-indigo-700 font-bold hover:text-white mt-4 hover:bg-indigo-700 lg:mt-0">Logout</a>   
+                   <a class="block text-md px-4  ml-2 py-2 rounded text-indigo-700 font-bold hover:text-white mt-4 hover:bg-indigo-700 lg:mt-0" href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                                 document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>  
             </div>
         </div>
-    
     </nav>
+
+    <!-- component -->
+<div class="bg-gradient-to-bl from-blue-50 to-violet-50 flex items-center justify-center lg:h-screen">
+    <div class="container mx-auto mx-auto p-4">
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-4">
+        <!-- Replace this with your grid items -->
+        <div class="bg-white rounded-lg border p-4">
+          <img src="/image/SD.jpeg" alt="Placeholder Image" class="w-full h-48 rounded-md object-cover">
+          <div class="px-1 py-4">
+            <div class="font-bold text-xl mb-2">SEKOLAH DASAR (SD)</div>
+            <p class="text-gray-700 text-base">
+              Menyediakan akses video pembelajaran, rangkuman materi dan latihan soal dari kelas 1 - 6 SD
+            </p>
+          </div>
+          <div class="px-1 py-4">
+            <a href="#" class="text-blue-500 hover:underline">Lihat Kelas</a>
+          </div>
+        </div>
+        <div class="bg-white rounded-lg border p-4">
+          <img src="/image/SMP.jpeg" alt="Placeholder Image" class="w-full h-48 rounded-md object-cover">
+          <div class="px-1 py-4">
+            <div class="font-bold text-xl mb-2">SEKOLAH MENENGAN PERTAMA (SMP)</div>
+            <p class="text-gray-700 text-base">
+                Menyediakan akses video pembelajaran, rangkuman materi dan latihan soal dari kelas 7 - 9 SMP
+            </p>
+          </div>
+          <div class="px-1 py-4">
+            <a href="#" class="text-blue-500 hover:underline">Lihat Kelas</a>
+          </div>
+        </div>
+        <div class="bg-white rounded-lg border p-4">
+          <img src="/image/SMA-SMK.jpeg" alt="Placeholder Image" class="w-full h-48 rounded-md object-cover">
+          <div class="px-1 py-4">
+            <div class="font-bold text-xl mb-2">SEKOLAH MENENGAH ATAS (SMA)</div>
+            <p class="text-gray-700 text-base">
+              TMenyediakan akses video pembelajaran, rangkuman materi dan latihan soal dari kelas 10 - 12 SMA
+            </p>
+          </div>
+          <div class="px-1 py-4">
+            <a href="#" class="text-blue-500 hover:underline">Lihat Kelas</a>
+          </div>
+        </div>
+        <div class="bg-white rounded-lg border p-4">
+          <img src="/image/SMA-SMK.jpeg" alt="Placeholder Image" class="w-full h-48 rounded-md object-cover">
+          <div class="px-1 py-4">
+            <div class="font-bold text-xl mb-2">SEKOLAH MENENGAH KEJURUAN (SMK)</div>
+            <p class="text-gray-700 text-base">
+                Menyediakan akses video pembelajaran, rangkuman materi dan latihan soal dari kelas 10 - 12 SMK
+            </p>
+          </div>
+          <div class="px-1 py-4">
+            <a href="#" class="text-blue-500 hover:underline">Lihat Kelas</a>
+          </div>
+        </div>
+        <!-- Add more items as needed -->
+      </div>
+    </div>
+  </div>
